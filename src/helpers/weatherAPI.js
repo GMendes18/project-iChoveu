c953468036cc463b7ee0c9499679fce254cbe7a9
Gabriel <gabri.mevial@gmail.com>
2023-06-29 18:30:36 -0300

    primeiro requisito searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,20 @@
 // Remova os comentários a medida que for implementando as funções
+const weatherToken = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const weatherAPI = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${weatherToken}&q=${term}`;
+  try {
+    const result = await fetch(weatherAPI);
+    const data = await result.json();
+    if (!data.lenght) {
+      window.alert('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    return [];
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+  //   seu código aqui
 };
